7c8d3af24272ad7b95aa93057f33cd6b89cf48bc
MrYen2-0 <223225@ids.upchiapas.edu.mx>
2023-10-22 23:49:17 -0600

    FINALMETE SE ARREGLO (un poco )

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,41 @@
-import styles from './page.module.css';
-
+import './globals.css';
 function Page() {
   return (
     <div>
-      <div className="v1_21">
-      <div className="v1_160">
-        <div className="v1_161"></div>
-        <div className="v1_204">Menu</div>
-        <div className="v1_205">Galeria</div>
-        <div className="v1_206">Abogados</div>
-        <div className="v1_207">Publicaciones</div>
-        <div className="v1_208">Contactos</div>
-        <div className="v1_209"></div>
-        <div className="v1_210"></div>
-        <div className="v804_44"></div>
-        <div className="v818_288"></div>
-      </div>
-      <div className="v1_22">Garcia & </div>
-      <div className="v1_23">Caceres Gonzales</div>
-      <div className="v1_24"></div>
-      <div className="v1_53">
-        <div className="v1_54"></div>
-        <div className="v1_55">Acerca de la empresa</div>
-        <div className="v1_56"></div>
-        <div className="v1_57"></div>
-        <div className="v1_58"></div>
-        <div className="v1_59">
-          <div className="v1_60"></div>
-          <div className="v1_61">
-            <div className="v1_62">Leer mas</div>
-            <div className="name"></div>
+      <div className="Contenedor-global">
+      <div className="contenedor-header">
+  <div className="v1_161"></div>
+  <div className="menu-item">Menu</div>
+  <div className="menu-item">Galeria</div>
+  <div className="menu-item">Abogados</div>
+  <div className="menu-item">Publicaciones</div>
+  <div className="menu-item">Contactos</div>
+  <div className="icono"></div>
+  <div className="imagen"></div>
+</div>
+<div className="contenedor-titulo">
+  <div className="titulos">
+    <div className="v1_22">Garcia & </div>
+    <div className="v1_23">Caceres Gonzales</div>
+  </div>
+  <div className="v1_24"></div>
+</div>
+        <div className="contenedor-info">
+          <div className="v1_55">Acerca de la empresa</div>
+          <div className="v1_56"></div>
+          <div className="v1_57"></div>
+          <div className="v1_58"></div>
+          <div className="v1_59">
+           <div className="v1_61">
+              <div className="v1_62">Leer mas</div>
           </div>
         </div>
         <div className="v1_64">
           Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry... Lorem Ipsum is simply dummy text of the printing and typesetting
           industry...
         </div>
       </div>
@@ -57,45 +59,43 @@ function Page() {
         <div className="v1_92"></div>
         <div className="v1_93"></div>
         <div className="v1_94"></div>
+        <div className="texto-oficina">Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry... Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...</div>
       </div>
-      <div className="v1_95">
-        <div className="v1_96">
-          <div className="v1_97">
-          <div className="v1_117">512.333.2222</div>
-      <div className="v1_118">sampleemail@gmail.com</div>
+      <div className="contenedor-footer">
+        <div className="footer-container">
+          <div className="footer-info">
+      <div className="v1_125">Informacion</div>
       <div className="v1_120">Menu</div>
       <div className="v1_121">Galeria</div>
       <div className="v1_122">Abogado</div>
       <div className="v1_123">Contactos</div>
       <div className="v1_124">Publicaciones</div>
-      <div className="v1_125">Informacion</div>
+      </div>
+
+      <div className="footer-info">
       <div className="v1_126">Contactos</div>
-      <div className="v1_130"></div>
-      <div className="v1_131">
-        <div className="v1_132">
-          <div className="v1_133"></div>
-        </div>
-      </div>
-      <div className="v1_134">
-        <div className="v1_135"></div>
-      </div>
       <div className="v1_136">1234 Sample Street Austin Texas 78704</div>
-          </div>
-          <div className="v1_98"></div>
-          <div className="v1_99">© 2021 All Rights Reserved</div>
-        </div>
-        <div className="v1_101">
+      <div className="v1_117">512.333.2222</div>
+      <div className="v1_118">sampleemail@gmail.com</div>
+      </div>
+
+      <div className="footer-info">
           <div className="v1_102">Medios sociales</div>
           <div className="v1_104"></div>
           <div className="v1_106"></div>
         </div>
+        </div>
+
+          <div className="v1_98"></div>
+          <div className="v1_99">© 2021 All Rights Reserved</div>
+        
       </div>
     </div>
-    <div className="v807_74">
-        <div className="v807_66"></div>
-        <div className="v805_63"></div>
-      </div>
-      
       </div>
   );
 }
